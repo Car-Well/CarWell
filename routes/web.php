b61e0e38a7fc545a4d509eabae7ef31cb848e976
Enzo Bariotto <enzobariotto@gmail.com>
2026-03-12 01:07:54 -0300

    Alterando os nomes da páginas para home

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste', function () {
-    return view('teste');
+Route::get('/home', function () {
+    return view('home');
 });
