fff1b12c83183b1b80a269ede34adf77eece58f3
Enzo Bariotto <enzobariotto@gmail.com>
2026-04-13 10:46:30 -0300

    feat: Criada a Mailable e o template de verificacao de email

---
+++ app/Mail/CodigoVerificacao.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CodigoVerificacao extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public string $code;
+
+    public function __construct(string $code)
+    {
+        $this->code = $code;
+    }
+
+    public function build(): self
+    {
+        return $this->subject('Confirme seu e-mail – CarWell')
+                    ->view('emails.verificacao');
+    }
+}
