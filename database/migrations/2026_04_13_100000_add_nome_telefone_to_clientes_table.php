ee3246056cb4e068e1e1f03179536161e7c2c5f0
Enzo Bariotto <enzobariotto@gmail.com>
2026-04-13 10:50:49 -0300

    feat: Alterada a migration para integrar telefone

---
+++ database/migrations/2026_04_13_100000_add_nome_telefone_to_clientes_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNomeTelefoneToClientesTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('clientes', function (Blueprint $table) {
+            $table->string('telefone', 20)->nullable()->after('name');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('clientes', function (Blueprint $table) {
+            $table->dropColumn('telefone');
+        });
+    }
+}
